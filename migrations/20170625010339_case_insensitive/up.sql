bee119fdd445f95a91ccfdb9630e1529f095598e
Ram <quadrupleslap@gmail.com>
2017-06-25 12:31:11 +1000

    no clue

--- migrations/20170625010339_case_insensitive/up.sql
+++ migrations/20170625010339_case_insensitive/up.sql
@@ -4,3 +4,8 @@ ALTER TABLE users ALTER COLUMN first_name TYPE CITEXT;
 ALTER TABLE users ALTER COLUMN last_name TYPE CITEXT;
 ALTER TABLE users ALTER COLUMN username TYPE CITEXT;
 ALTER TABLE users ALTER COLUMN email TYPE CITEXT;
+
+ALTER TABLE users DROP CONSTRAINT users_email_key;
+CREATE UNIQUE INDEX users_email_key ON users (lower(email));
+ALTER TABLE users DROP CONSTRAINT users_username_key;
+CREATE UNIQUE INDEX users_username_key ON users (lower(username));
